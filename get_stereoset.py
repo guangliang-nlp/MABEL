f9ddb4aac9e4f525af302e026f038be96dda1c4f
Guangliang Liu <liuguangliang89@gmail.com>
2023-11-30 19:32:17 -0500

    Update get_stereoset.py

--- get_stereoset.py
+++ get_stereoset.py
@@ -5,7 +5,7 @@ import transformers
 transformers.logging.set_verbosity_error()
 ratios = ['0.0', '0.25', '0.5', '0.75', '1.0']
 tasks = ['mnli', 'jigsaw-gender', 'snli']
-models = ['bert-base-uncased', 'roberta-base']
+models = ['bert-base-uncased']#, 'roberta-base']
 methods = ['vanillia', 'debiased']
 
 if __name__ == "__main__":
