524962bdd4d0d0cb7f2b4586af8711e0f7f03046
Guangliang Liu <liuguangliang89@gmail.com>
2023-12-01 14:59:11 -0500

    u

---
+++ log_process.py
@@ -0,0 +1,18 @@
+sen_list = []
+sen = ""
+for line in open("crowspair.log"):
+
+    if line.strip().startswith("/scratch0/liuguan5/models/cda/"):
+        sen = sen + line.strip().split("/")[-1]
+        #print(sen)
+
+    elif line.strip().startswith("Metric score"):
+        score = line.strip().split(":")[-1].strip()
+        print(line,score)
+        sen = sen + "\t" + score
+        sen_list.append(sen)
+        sen = ""
+
+with open("crowspair.score.log",'w') as writer:
+    writer.write('\n'.join(sen_list))
+
