d7b7bd5cfa97ceffa640dbb51ed581647e5b6db6
Guangliang Liu <liuguangliang89@gmail.com>
2023-11-30 19:12:42 -0500

    Update get_stereoset.py

--- get_stereoset.py
+++ get_stereoset.py
@@ -20,4 +20,4 @@ if __name__ == "__main__":
 
                         model_file = model_file.replace("*",str(i))
                         print(model_file)
-                        #os.system("python -m benchmark.intrinsic.stereoset.predict --model_name_or_path {0} --model {1} && python -m benchmark.intrinsic.stereoset.eval".format(model_file, model))
+                        os.system("python -m benchmark.intrinsic.stereoset.predict --model_name_or_path {0} --model {1} && python -m benchmark.intrinsic.stereoset.eval".format(model_file, model))
