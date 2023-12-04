5dcca3fa6715e1963a93eea90d0c6861f638fb06
Guangliang Liu <liuguangliang89@gmail.com>
2023-12-04 02:29:22 -0500

    u

--- get_stereoset.py
+++ get_stereoset.py
@@ -1,3 +1,4 @@
+import glob
 import os
 
 import transformers
@@ -7,7 +8,7 @@ ratios = [100,500,1000,5000]#['0.0', '0.25', '0.5', '0.75', '1.0']
 tasks = ['mnli', 'jigsaw-gender', 'snli']
 models = ['bert-base-uncased']
 methods = ['debiased']#'vanillia',
-
+'''
 if __name__ == "__main__":
 
     prefix = "/scratch0/liuguan5/models/cda/iclr24/"
@@ -24,3 +25,22 @@ if __name__ == "__main__":
                         model_file = model_file.replace("*",str(i))
                         print(model_file)
                         os.system("python -m benchmark.intrinsic.stereoset.predict --model_name_or_path {0} --model {1} && python -m benchmark.intrinsic.stereoset.eval".format(model_file, model))
+
+'''
+if __name__ == "__main__":
+    tasks = ['snli', 'sts', 'biasbios']
+    models = ['bert-base-uncased']
+    methods = ['debiased','normal','ours','ear']  # 'vanillia',
+    prefix = "/scratch0/liuguan5/models/cda/iclr24/"
+    for model in models:
+        for ratio in ratios:
+            for task in tasks:
+
+                for method in methods:
+
+                    for i in [1,42,100]:
+                        model_file = prefix + 'test.' + task + "." + model + "." + method + ".*.ckpt"
+
+                        for model_ in glob.glob(model_file):
+                            print(model_)
+                            os.system("python -m benchmark.intrinsic.stereoset.predict --model_name_or_path {0} --model {1} && python -m benchmark.intrinsic.stereoset.eval".format(model_, model))
--- log_process.py
+++ log_process.py
@@ -21,22 +21,31 @@ with open("crowspair.score.log",'w') as writer:
 
 sen_list = []
 sen=""
-for line in open("stereoset.bert.log"):
 
-    if line.strip().startswith("/scratch0/liuguan5/models/cda/"):
-        sen = sen + line.strip().split("/")[-1]
-        #print(sen)
+try:
+    for line in  open("datasize.stereoset.log"):
 
-    elif line.strip().startswith("LM Score"):
-        score = line.strip().split(":")[-1].strip()
 
-        sen = sen + "\t" + score
+            if "24464"  in line:
+                continue
+            print(line)
 
-    elif line.strip().startswith("SS Score"):
-        score = line.strip().split(":")[-1].strip()
-        sen = sen + "\t" + score
-        sen_list.append(sen)
-        sen = ""
+            if line.strip().startswith("/scratch0/liuguan5/models/cda/"):
+                sen = sen + line.strip().split("/")[-1]
+                #print(sen)
 
-with open("stereoset.bert.score.log",'w') as writer:
+            elif line.strip().startswith("LM Score"):
+                score = line.strip().split(":")[-1].strip()
+
+                sen = sen + "\t" + score
+
+            elif line.strip().startswith("SS Score"):
+                score = line.strip().split(":")[-1].strip()
+                sen = sen + "\t" + score
+                sen_list.append(sen)
+                sen = ""
+except: pass
+
+
+with open("stereoset.bert.datasize.log",'w') as writer:
     writer.write('\n'.join(sen_list))
