068b76f7e6bf0e6e1847575890e7a5d742010d5f
Guangliang Liu <liuguangliang89@gmail.com>
2023-12-04 02:34:13 -0500

    Update get_stereoset.py

--- get_stereoset.py
+++ get_stereoset.py
@@ -4,10 +4,10 @@ import os
 import transformers
 
 transformers.logging.set_verbosity_error()
-ratios = [100,500,1000,5000]#['0.0', '0.25', '0.5', '0.75', '1.0']
+ratios = [100, 500, 1000, 5000]  # ['0.0', '0.25', '0.5', '0.75', '1.0']
 tasks = ['mnli', 'jigsaw-gender', 'snli']
 models = ['bert-base-uncased']
-methods = ['debiased']#'vanillia',
+methods = ['debiased']  # 'vanillia',
 '''
 if __name__ == "__main__":
 
@@ -30,17 +30,16 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     tasks = ['snli', 'sts', 'biasbios']
     models = ['bert-base-uncased']
-    methods = ['debiased','normal','ours','ear']  # 'vanillia',
+    methods = ['debiased', 'normal', 'ours', 'ear']  # 'vanillia',
     prefix = "/scratch0/liuguan5/models/cda/iclr24/"
-    for model in models:
-        for ratio in ratios:
-            for task in tasks:
 
-                for method in methods:
-
-                    for i in [1,42,100]:
-                        model_file = prefix + 'test.' + task + "." + model + "." + method + ".*.ckpt"
-
-                        for model_ in glob.glob(model_file):
-                            print(model_)
-                            os.system("python -m benchmark.intrinsic.stereoset.predict --model_name_or_path {0} --model {1} && python -m benchmark.intrinsic.stereoset.eval".format(model_, model))
+    for model_ in glob.glob(r'/scratch0/liuguan5/models/cda/iclr24/test.*ours*bert-base-uncased*.ckpt'):
+        print(model_)
+        os.system(
+            "python -m benchmark.intrinsic.stereoset.predict --model_name_or_path {0} --model {1} && python -m benchmark.intrinsic.stereoset.eval".format(
+                model_,'bert-base-uncased'))
+    for model_ in glob.glob(r'/scratch0/liuguan5/models/cda/iclr24/test.*debiased*bert-base-uncased*.ckpt'):
+        print(model_)
+        os.system(
+            "python -m benchmark.intrinsic.stereoset.predict --model_name_or_path {0} --model {1} && python -m benchmark.intrinsic.stereoset.eval".format(
+                model_,'bert-base-uncased'))
